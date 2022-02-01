b81a02f95c5af4819e9c4df4a90cc3e9d8114139
Cesar Mak <ckmakcesar@gmail.com>
2022-02-01 11:13:41 +0100

    Containerization

--- server/src/server.js
+++ server/src/server.js
@@ -7,12 +7,18 @@ const { logger } = require('./utils/logger');
 const { SERVER_PORT, SERVER_HOST } = require('./env');
 
 const app = express();
+const publicPath = express.static(path.join(__dirname, '..', 'public'), { redirect: false });
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // as body parser for incoming request
 
 app.use((req, _, next) => { logger.info(`${req.method} ${req.originalUrl}`); next(); });
+
+// For all urls that are NOT /api/, serve with the built frontend client (located at ./server/public)
+const indexPath = path.join(__dirname, '..', 'public', 'index.html');
+app.use(publicPath);
+app.get(/^\/(?!api\/).*/, (_, res) => { res.sendFile(indexPath) });
 
 app.use('/api/v1/reservations', reservationController);
 
